1543b8d0c6176c9169e56720a48b0945ff776269
didrux <60069950+didrux@users.noreply.github.com>
2020-11-13 21:28:05 +0200

    Update main.c
    fully workable code but using scanf

--- main.c
+++ main.c
@@ -3,23 +3,43 @@
 
 int main()
 {   int k;
-    int ak[1000];
+    int m;
+    int N;
+    scanf("%d%d", &N,&m);
+    int ak[N*m];
     int maxx[1000];
     int max;
     max=0;
-    int m;
-    int n;
+    int n=1;
     int b;
-    int N;
-    scanf("%d%d", &N,&m);
-
-    for(int n=1;n<N;n++) {
+    int min=32767;
+    for(n=1;n<N;n++) {
 
     for(k=(m*(n-1)+1),b=(n*m),k=1;k<=b;k++){
             printf("Enter value ");
             scanf("%d", &ak[k]);
             if (ak[k]>max) {
                 max=ak[k];
+                }
+
+    }
+    maxx[n]=max;
+    printf("Well, the new n started. \n");
+    printf("%d \n",max);
+    max=0;
+    if (maxx[n]<min) {
+        min=maxx[n];
+    }
+    }
+
+    printf("%d ",min);
+
+
+
+    return 0;
+
+}
+max=ak[k];
                 }
 
     }
